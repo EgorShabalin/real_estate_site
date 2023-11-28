80bac4799b1d0798f14fcd11f15bfc9ff0ff837b
egor <egor.shabalin.wd@gmail.com>
2023-11-28 21:20:17 +0300

    secrets created

--- my_project/my_project/settings.py
+++ my_project/my_project/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+import os, dotenv
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,12 +22,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-cq#cg0t*)+p#z$li4h0@*jbegb&0_sc!jg^%h(+yladsw!*kf3"
+dotenv_path = BASE_DIR / "secrets/env"
+dotenv.load_dotenv(dotenv_path)
+
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ["ALLOWED_HOSTS"]
 
 
 # Application definition
