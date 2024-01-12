27d8d0b7e26254c423eaad26e54a6b473175ec91
egor <egor.shabalin.wd@gmail.com>
2024-01-12 13:37:42 +0300

    property_detail design fixes

--- my_project/my_site/models.py
+++ my_project/my_site/models.py
@@ -70,29 +70,72 @@ class Parking(models.Model):
 
 
 class Property(models.Model):
-    title = models.CharField(max_length=250, verbose_name=_("title"))
-    price = models.CharField(max_length=10)
+    title = models.CharField(
+        max_length=250,
+        verbose_name=_("title"),
+    )
+    price = models.CharField(
+        max_length=10,
+    )
     province = models.CharField(
-        max_length=50, choices=IL_LIST, default="Antalya", blank=False, null=False
+        max_length=50,
+        choices=IL_LIST,
+        default="Antalya",
+        blank=False,
+        null=False,
     )
     district = models.CharField(
-        max_length=50, choices=ANTALYA_DIST_LIST, blank=True, null=True
+        max_length=50,
+        choices=ANTALYA_DIST_LIST,
+        blank=True,
+        null=True,
     )
-    address = models.CharField(max_length=250, blank=True, null=True)
-    floor_number = models.DecimalField(max_digits=2, decimal_places=0, default=0)
-    total_floors = models.DecimalField(max_digits=2, decimal_places=0, default=0)
+    address = models.CharField(
+        max_length=250,
+        blank=True,
+        null=True,
+    )
+    floor_number = models.DecimalField(
+        max_digits=2,
+        decimal_places=0,
+        default=0,
+    )
+    total_floors = models.DecimalField(
+        max_digits=2,
+        decimal_places=0,
+        default=0,
+    )
     parking = models.ForeignKey(
-        Parking, on_delete=models.CASCADE, null=False, default=[1]
+        Parking,
+        on_delete=models.CASCADE,
+        null=False,
+        default=[1],
     )
-    rooms = models.CharField(max_length=200)
-    area = models.CharField(max_length=200)
+    rooms = models.CharField(
+        max_length=200,
+    )
+    area = models.CharField(
+        max_length=200,
+    )
     heating = models.ForeignKey(
-        Heating, on_delete=models.CASCADE, null=False, default=[1]
+        Heating,
+        on_delete=models.CASCADE,
+        null=False,
+        default=[1],
     )
     elevator = models.CharField(
-        max_length=50, choices=ELEVATOR, default="No", blank=False, null=False
+        max_length=50,
+        choices=ELEVATOR,
+        default="No",
+        blank=False,
+        null=False,
     )
-    frontage = models.CharField(max_length=50, choices=FRONTAGE, blank=True, null=True)
+    frontage = models.CharField(
+        max_length=50,
+        choices=FRONTAGE,
+        blank=True,
+        null=True,
+    )
     iskan = models.CharField(
         max_length=50,
         choices=ISKAN,
@@ -116,19 +159,32 @@ class Property(models.Model):
     )
     description = models.TextField()
     category = models.ForeignKey(
-        Category, on_delete=models.CASCADE, null=False, default=[1]
+        Category,
+        on_delete=models.CASCADE,
+        null=False,
+        default=[1],
     )
     type_of_deal = models.CharField(
-        max_length=10, choices=DEAL_TYPES, blank=False, null=False, default="SALE"
+        max_length=10,
+        choices=DEAL_TYPES,
+        blank=False,
+        null=False,
+        default="SALE",
     )
-    active = models.BooleanField(default=True)
+    active = models.BooleanField(
+        default=True,
+    )
     bookmark = models.ManyToManyField(
         User,
         related_name="bookmarks",
         blank=True,
     )
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True,
+    )
 
     def __str__(self):
         return (
@@ -148,10 +204,37 @@ class Property(models.Model):
 
 class Photo(models.Model):
     property = models.ForeignKey(
-        Property, on_delete=models.CASCADE, related_name="photos"
+        Property,
+        on_delete=models.CASCADE,
+        related_name="photos",
     )
     photo = models.ImageField(upload_to="photos/")
 
 
 class Profile(models.Model):
-    pass
+    current_user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+    )
+    ava = models.ImageField(
+        default=None,
+        upload_to="avatars",
+        blank=True,
+        null=True,
+    )
+    bio = models.TextField(
+        blank=True,
+        null=True,
+    )
+    phone = models.CharField(
+        max_length=15,
+    )
+    follows = models.ManyToManyField(
+        "self",
+        related_name="followed_by",
+        symmetrical=False,
+        blank=True,
+    )
+
+    def __str__(self) -> str:
+        return self.current_user.username
